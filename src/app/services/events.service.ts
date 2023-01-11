58eedd22be5369cc5f7430bfe278fbcd38c15bfc
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2023-01-11 17:11:47 -0500

    contentful fixes

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,33 +1,45 @@
 import { Injectable } from '@angular/core';
-import { createClient, Entry} from 'contentful'
+import { createClient, Entry } from 'contentful';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventsService {
-
   private client = createClient({
-    space: environment.contentfulVolidaEvents.spaceId,
-    accessToken: environment.contentfulVolidaEvents.token
+    space: environment.contentfulVolidaFilms.spaceId,
+    accessToken: environment.contentfulVolidaFilms.token,
   });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllEvents(query?: object): Promise<Entry<any>[]> {
-
-    return this.client.getEntries(Object.assign({
-      content_type: environment.contentfulVolidaEvents.contentTypeIds.product
-    }, query))
-      .then(res => res.items)
+    return this.client
+      .getEntries(
+        Object.assign(
+          {
+            content_type:
+              environment.contentfulVolidaFilms.contentTypeIds.product,
+          },
+          query
+        )
+      )
+      .then((res) => res.items);
   }
 
-  getEvent(eventId: any) : Promise<Entry<any>>{
+  getEvent(eventId: any): Promise<Entry<any>> {
     // return this.http.post<{token: string, expiresIn: number, auth: any, error: any}>("https://volida-be.herokuapp.com/api/get-event-details", {id:eventId, userId:localStorage.getItem('_id')});
-    return this.client.getEntries(Object.assign({
-    content_type: environment.contentfulVolidaEvents.contentTypeIds.product
-    }, {'sys.id': eventId}))
-    .then(res =>  res.items[0]);
+    return this.client
+      .getEntries(
+        Object.assign(
+          {
+            content_type:
+              environment.contentfulVolidaFilms.contentTypeIds.product,
+          },
+          { 'sys.id': eventId }
+        )
+      )
+      .then((res) => res.items[0]);
   }
 }
