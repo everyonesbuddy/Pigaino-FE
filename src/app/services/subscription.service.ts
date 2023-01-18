85b053db27402ff6b79fc5de383e53ee6c5fbd10
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2023-01-17 23:19:43 -0500

    stripe succes page fix

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -40,7 +40,7 @@ export class SubscriptionService {
         if (response.subscriptions.length === 0) {
           this.router.navigate(['']);
         } else {
-          this.router.navigate(['/dashboard']);
+          this.router.navigate(['/events']);
         }
       });
   }
