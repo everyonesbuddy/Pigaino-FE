ad60f9a43090c83d9d403d5f2a7a801f314c682f
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2023-06-15 15:16:46 -0400

    add subscription to FE

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -75,8 +75,9 @@ export class SubscriptionService {
       .subscribe((response: any) => {
         for (let i = 0; i < response.subscriptions.length; i++) {
           if (
-            response.subscriptions.length > 0 &&
-            response.subscriptions[i].status === 'active'
+            (response.subscriptions.length > 0 &&
+              response.subscriptions[i].status === 'active') ||
+            'trialing'
           ) {
             accessGranted = true;
             break;
