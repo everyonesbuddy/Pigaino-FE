92586113fa87f218f7ed169a5c16c50303057b57
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2023-08-02 11:29:41 -0400

    Fe change to handle one time payments

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -75,9 +75,11 @@ export class SubscriptionService {
       .subscribe((response: any) => {
         for (let i = 0; i < response.subscriptions.length; i++) {
           if (
-            (response.subscriptions.length > 0 &&
-              response.subscriptions[i].status === 'active') ||
-            'trialing'
+            // (response.subscriptions.length > 0 &&
+            //   response.subscriptions[i].status === 'active') ||
+            // 'trialing'
+            response.subscriptions.length > 0 &&
+            response.subscriptions[i].status === 'succeeded'
           ) {
             accessGranted = true;
             break;
